585e0ec013428085800b018d357b38488cc4afd4
Matt Harris <711mattman@gmail.com>
2021-01-24 13:59:48 -0500

    Finish up the components files

--- employee/utils/API.js
+++ employee/utils/API.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+const BASEURL = "https://randomuser.me/api/?inc=gender,name,email,picture";
 
 
 export default {
     getUsers: function() {
-        return axios.get("https://randomuser.me/api/?results=200");
+        return axios.get(BASEURL);
     }
     
 }
