3710f664bae6fc7f94d905a81f04c32152527f86
Matt Harris <711mattman@gmail.com>
2021-02-24 12:00:57 -0500

    Added the components to the app.js file

--- employee/src/App.js
+++ employee/src/App.js
@@ -1,15 +1,22 @@
-import React, { Component } from "react";
-import logo from './logo.svg';
-import './App.css';
-import SearchResult from "./components/SearchResult";
+import React from "react";
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import "./App.css";
 
-
-class App extends Component {
-  render(){
-    return (
-      <SearchResult />
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+    <Wrapper>
+      <Header />
+      <Main />
+    </Wrapper>
+  </div>
+  )
+  
 }
+
+
+
 
 export default App;
